cd7a98d3255422d7d11554417153bbc229cf9e0e
Alexis Jacob <ferjacobka@gmail.com>
2026-07-25 19:45:54 -0400

    Chore: iniciando logica de cuenta con sus atributos

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Cuenta {
+    private String numeroCuenta;
+    private String pin;
+    private double saldo;
+    private ArrayList historial = new ArrayList();
+
+    public Cuenta() {
+    }
 }
