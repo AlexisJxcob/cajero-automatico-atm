36fbcec610e7c967695548bb3ea365b747dcec3c
Alexis Jacob <ferjacobka@gmail.com>
2026-07-25 19:59:27 -0400

    Chore: constructor y mejorando la logica de deposito de cuenta

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -4,21 +4,30 @@ public class Cuenta {
     private String numeroCuenta;
     private String pin;
     private double saldo;
-    private ArrayList historial = new ArrayList();
+    private ArrayList<String> historial = new ArrayList<>();
+
+    public Cuenta(String numeroCuenta, double saldo, String pin) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.pin = pin;
+    }
 
     // metodos
     // para depositar dinero en efectivo
-    public double depositar (double ingresoCantidad) {
-        return ingresoCantidad;
+    public void depositar(double ingresoCantidad) {
+        if (ingresoCantidad > 0) {
+         this.saldo += ingresoCantidad;
+         registraRetiro("Hizo un deposito de: $" + ingresoCantidad + " y su nuevo saldo actual es de: $" + this.saldo);
+        }
     }
 
     // retirar
-    public double retirar (double retiroCantidad) {
+    public double retirar(double retiroCantidad) {
         return retiroCantidad;
     }
 
     // registrar transaccion en el historial
-    public void registraRetiro (String mensaje){
-
+    public void registraRetiro(String mensaje) {
+    historial.add(mensaje);
     }
 }
